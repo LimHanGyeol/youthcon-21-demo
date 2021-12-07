b57878f4a7cea07bd2807ee0aebf00c4c3fe9131
LimHanGyeol <hang19663@gmail.com>
2021-12-07 23:13:21 +0900

    Initial project - second

---
+++ secondService/src/main/java/com/yoouthcon21/secondservice/SecondServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yoouthcon21.secondservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondServiceApplication.class, args);
+    }
+
+}
