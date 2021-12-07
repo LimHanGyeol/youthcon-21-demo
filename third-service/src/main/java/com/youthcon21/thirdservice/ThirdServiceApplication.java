6e3b4a318ceec7a89773bf26448697343d12cd6a
LimHanGyeol <hang19663@gmail.com>
2021-12-08 00:41:25 +0900

    Initial project - third

---
+++ third-service/src/main/java/com/youthcon21/thirdservice/ThirdServiceApplication.java
@@ -0,0 +1,13 @@
+package com.youthcon21.thirdservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdServiceApplication.class, args);
+    }
+
+}
